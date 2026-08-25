8b850705bcda95f59f39faef176bb8e7d568fdd4
nicoleroy06 <nicole06@vt.edu>
2026-08-25 17:38:55 -0400

    Clean project setup and data path

--- R/cohort_feature_engineering.R
+++ R/cohort_feature_engineering.R
@@ -1,3 +1,4 @@
+###############################################################################
 # Miles to Care: Transportation Barriers & Patient Journeys
 # ASA DataFest 2026
 #
@@ -6,4 +7,16 @@
 # This script prepares encounter-level healthcare data for analysis of
 # transportation barriers and patient journeys.
 #
-# Note: The original DataFest datasets are not included in this repository.
+# The original ASA DataFest healthcare datasets are not included in this
+# repository. Users should place authorized source files in a local data folder.
+###############################################################################
+
+library(tidyverse)
+library(data.table)
+library(lubridate)
+
+# Set the path to the folder containing the authorized DataFest source files.
+# Example:
+# data_dir <- "data/raw"
+
+data_dir <- "data/raw"
