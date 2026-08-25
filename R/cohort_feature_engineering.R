c08c63d718de3cfe58013e7be30c07cc2c852a13
nicoleroy06 <nicole06@vt.edu>
2026-08-25 17:37:41 -0400

    Create cohort feature engineering script

---
+++ R/cohort_feature_engineering.R
@@ -0,0 +1,9 @@
+# Miles to Care: Transportation Barriers & Patient Journeys
+# ASA DataFest 2026
+#
+# Cohort Building & Feature Engineering
+#
+# This script prepares encounter-level healthcare data for analysis of
+# transportation barriers and patient journeys.
+#
+# Note: The original DataFest datasets are not included in this repository.
